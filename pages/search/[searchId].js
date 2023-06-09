6243c0020a4f4668a8017e8ac8294523f28d342c
João <joao.magalhaes.s1805@gmail.com>
2023-06-09 20:15:26 -0300

    UIv0.5

--- pages/search/[searchId].js
+++ pages/search/[searchId].js
@@ -1,50 +1,68 @@
 //  mygamelist.net/search/searchId
-import GameCard from "../../components/GameCard"
+import React, { useState } from 'react';
+import GameCard from "../../components/GameCard";
 
-function SearchPage(){
-    const searchinput = 'searchId'
+function SearchPage() {
+  const searchInput = 'searchId';
+  const [platform, setPlatform] = useState('');
+  const [genre, setGenre] = useState('');
 
-    function handleClick() {
-        const options = document.getElementById("options");
-        if (options.style.display === "none") {
-          options.style.display = "block";
-        } else {
-          options.style.display = "none";
-        }
-      }
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    return(
-        <div className="searchPage">
-            <h1 className="searchInput">"{searchinput}"</h1> {/* Aqui deve ta o que foi pesquisado (nome/categoria do jogo), ou seja o searchId */}
-            <div className="filters">
-                <button id="plataformsBtn">Plataforms</button> {/*Filtros de pesquisa, me besiei nos que a gente considerou na criação do prototipo das paginas, não sei como funciona então deixei butões que não fazem nada */}
-                <div id="options">
-                    <p>PC</p>
-                    <p>Playstation</p>
-                    <p>Nintendo</p>
-                    <p>Xbox</p>
-                </div>
-                <button id="plataformsBtn">Genre</button>
-                <div id="options">
-                    <p>Action</p>
-                    <p>Fps</p>
-                    <p>RPG...</p>
-                </div>
-            </div>
-            <div className="cardsDisplay">
-                <GameCard/> {/* Aqui vão ser um card para cada jogo que tenha um resultado para a pesquisa do usuário */}
-                <GameCard/>
-                <GameCard/>
-                <GameCard/>
-                <GameCard/>
-                <GameCard/>
-                <GameCard/>
-                <GameCard/>
-                <GameCard/>
-            </div>
-            <script src="./script"></script>
-        </div>
-    )
+    if (platform !== '' && genre !== '') {
+        console.log(platform, genre);
+    } else if (platform !== '') {
+        console.log(platform);
+    } else if (genre !== '') {
+        console.log(genre);
+    }
+  };
+
+  const handlePlatformChange = (event) => {
+    setPlatform(event.target.value);
+  };
+
+  const handleGenreChange = (event) => {
+    setGenre(event.target.value);
+  };
+
+  return (
+    <div className="searchPage">
+      <h1 className="searchInput">"{searchInput}"</h1>
+      <form className="filters" onSubmit={handleSubmit}>
+        <select name="platforms" id="platform-id" value={platform} onChange={handlePlatformChange}>
+          <option value="" disabled>Select Platform</option>
+          <option value="PC">PC</option>
+          <option value="PlayStation">PlayStation</option>
+          <option value="Nintendo">Nintendo</option>
+          <option value="Xbox">Xbox</option>
+        </select>
+
+        <select name="genres" id="genre-id" value={genre} onChange={handleGenreChange}>
+          <option value="" disabled>Select Genre</option>
+          <option value="Action">Action</option>
+          <option value="FPS">FPS</option>
+          <option value="RPG">RPG</option>
+        </select>
+
+        <input type="submit" name="submit" value="Apply filters" />
+      </form>
+
+      <div className="cardsDisplay">
+        {/* Renderiza os cards em base nos resultados da busca */}
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+      </div>
+    </div>
+  );
 }
 
 export default SearchPage;
